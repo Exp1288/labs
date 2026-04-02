558b15661144676e6e19ed65cb3cefcfd1db126d
Amir <madykovamir@gmail.com>
2026-04-02 13:06:00 +0600

    +translating lab1_2

--- lab1_2.py
+++ lab1_2.py
@@ -1,5 +1,5 @@
-print("Программа для вычисления значения y=2^x1+4^x2")
-x1 = int(input("Введите значение x1: "))
-x2 = int(input("Введите значение x2: "))
+print("Program to calculate the value of y=2^x1+4^x2")
+x1 = int(input("Enter the value of x1: "))
+x2 = int(input("Enter the value of x2: "))
 y = 2**x1 + 4**x2
-print("Значение y:", y)
+print("The value of y is:", y)
