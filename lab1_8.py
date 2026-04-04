540e14ed7f5f9abfe5494bb2e408f421c266a036
Amir <madykovamir@gmail.com>
2026-04-04 17:38:01 +0600

    improving the lab1_8

--- lab1_8.py
+++ lab1_8.py
@@ -2,9 +2,15 @@ print("The program determines is triangle equalateral or not")
 a = float(input("Enter the first side of the triangle: "))
 b = float(input("Enter the second of the triangle: "))
 c = float(input("Enter the third of the triangle: "))
-if a == b or b == c or c == a:
-    print("The triangle is equalateral")
-elif a == b and c != b or a:
-    print("The triangle is isosceles")
-else: 
-    print("The triangle is scalene")
+#does the triangle exist?
+if a+b>c and a+c>b and b+c>a:
+    if a == b and b == c and c == a:
+        print("The triangle is equalateral")
+    elif a == b and c != b and c != a:
+        print("The triangle is isosceles")
+    elif a != b and c != b and a != c: 
+        print("The triangle is scalene")
+    else:
+        print("Something gone wrong")
+else:
+    print("That triangle doesn't exist")
