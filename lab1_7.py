810eefbbd2507c408888f6a0b88dab9195527ba1
Amir <madykovamir@gmail.com>
2026-04-04 15:01:51 +0600

    +lab1_7

---
+++ lab1_7.py
@@ -0,0 +1,8 @@
+print("The program determines if two numbers in a pair are equal")
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = float(input("Enter the third number: "))
+if a == b or b == c or c == a:
+    print("A pair of numbers is equal")
+else: 
+    print("A pair of numbers isn's equal")
