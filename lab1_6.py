ea9058b7b78e4696ec7a0d0561b536edb1550370
Amir <madykovamir@gmail.com>
2026-04-04 15:04:28 +0600

    lab1_6 was translated

--- lab1_6.py
+++ lab1_6.py
@@ -1,5 +1,5 @@
-A = int(input('Введите объем в байтах: '))
+A = int(input('Enter the size in bytes: '))
 kb = A / 1024
 mg = kb / 1024
 gb = mg / 1024
-print(f"Мегабайты, {mg}, килобайты, {kb}, гигабайты {gb}")
+print(f"Megabytes, {mg}, kilobytes, {kb}, gigabytes {gb}")
