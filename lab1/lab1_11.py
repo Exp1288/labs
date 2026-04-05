72c36c024cad428159b2fa79048509076937b7c6
Amir <madykovamir@gmail.com>
2026-04-05 10:56:41 +0600

    mistake on lab1_11

--- lab1/lab1_11.py
+++ lab1/lab1_11.py
@@ -1,6 +1,6 @@
 r = int(input("Enter the radius of the circle: "))
 s = int(input("Enter the side of the square: "))
-if 2*r == s:
+if 2*r <= s:
     print("The circle fits in the square")
 else:
     print("The circle doesn't fit in the square")
