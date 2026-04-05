30496a958cabbdcd3a62866c268bd228a4c1cc6c
Amir <madykovamir@gmail.com>
2026-04-05 14:33:05 +0600

    +lab1_16

---
+++ lab1/lab1_16.py
@@ -0,0 +1,10 @@
+a = input("Enter a four-digit number: ")
+b = list(a)
+b.reverse()
+if len(b) > 4 or len(b) < 4:
+    print("that number is bigger or lower than four-digit")
+    quit()
+if list(a) == b:
+    print("It's a palindrome")
+else:
+    print("It isn's a palindrome")
