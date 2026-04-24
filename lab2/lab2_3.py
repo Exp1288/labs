8cbdb5673c3e5297bbbeebc42857732f3c99b7ae
Amir <madykovamir@gmail.com>
2026-04-24 22:26:23 +0600

    +lab2_3

---
+++ lab2/lab2_3.py
@@ -0,0 +1,3 @@
+dict = {1:'Monday', 2:'Tuesday', 3:'Wednesday', 4:'Thursday', 5:'Friday', 6:'Saturday', 7:"Sunday"}
+a = int(input("Enter a number of the day of the week: "))
+print(f"Today is {dict[a]}")
