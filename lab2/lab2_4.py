bfeefcc192565c37a413f5d80c4bbc799d33c547
Amir <madykovamir@gmail.com>
2026-05-08 16:43:22 +0600

    +lab2_4

---
+++ lab2/lab2_4.py
@@ -0,0 +1,3 @@
+dict = {1:'Write a note', 2:'Go to the park', 3:'Get a package', 4:'Go to the gym', 5:'Sit at home', 6:'Weekend', 7:"Studying"}
+a = int(input("Enter a number of the day of the week: "))
+print(f"Today's exercise is: {dict[a]}")
