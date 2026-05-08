0aac769d1aba970729eb7e2bbb41c4f89d5ebb04
Amir <madykovamir@gmail.com>
2026-05-08 17:32:07 +0600

    lab2_7

---
+++ lab2/lab2_7.py
@@ -0,0 +1,13 @@
+from random import randint
+import random 
+r = []
+a = 0
+for i in range(1, 20):
+    r.append(random.randint(1, 10))
+for i in range(1, 10):
+    if r[i] % 3 == 0:
+        a += r[i]
+    else:
+        pass
+print(r)
+print("Summ: ", a)
