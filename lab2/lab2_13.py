04f62bda2cd4b709977e2af201042baf31c58f16
Amir <madykovamir@gmail.com>
2026-05-10 13:34:43 +0600

    mistake in lab2_13

--- lab2/lab2_13.py
+++ lab2/lab2_13.py
@@ -1,9 +1,10 @@
-a = 0
-b = int(input("Enter the end: "))
-for i in range(3, b):
-    if i % 3 == 0:
-        a += 1
+a = 3
+c = 0
+while a < 200:
+    a += 1
+    if a % 3 == 0:
+        c += 1
     else:
         pass
-print("There's quantity of numbers that are multiples of 3, from 3 to", b)
-print(a)
+print("There's quantity of numbers that are multiples of 3 and aren't bigger than 200: ")
+print(c)
